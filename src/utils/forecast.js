754eef0f160ec38ce217aed590488da01dda45f4
LNU <2097734@cognizant.com>
2022-05-21 21:38:58 +0530

    Forecast changed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,7 @@ const forecast = (latitude, longitude, callback) =>{
         }
         else
         {
-            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees`)
+            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees. Humidity is ${body.current.humidity}`)
         }
     })
 }
